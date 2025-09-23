d7301a9c3bb087c96a583d2c5a2de0290c2ea07c
wardpintens-spec <wardpintens@gmail.com>
2025-09-23 15:19:55 +0200

    Commit

--- week2/ASCII_tabel.java
+++ week2/ASCII_tabel.java
@@ -1,16 +1,26 @@
 package week2;
-
+import java.util.Scanner;
 public class ASCII_tabel {
     public static void main(String[] args) {
         int perRij = 6;
         int rij = 0;
-        int waarde = 32;
+        int beginwaarde;
+        int einwaarde;
 
-        while (waarde <= 383) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Geef beginwaarde: ");
+        beginwaarde = scan.nextInt();
+
+        System.out.print("Geef eindwaarde: ");
+        einwaarde = scan.nextInt();
+
+        scan.close();
+        while (beginwaarde <= einwaarde) {
             while (rij != perRij) {
-                System.out.print((char) waarde + "(" + waarde + ") ");
+                System.out.print((char) beginwaarde + "(" + beginwaarde + ") ");
                 rij++;
-                waarde++;
+                beginwaarde++;
             }
             rij = 0;
             System.out.print("\n");
