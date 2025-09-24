e30e1198b9b85e9728b01ab3ebd34304c6b2a067
wardpintens-spec <wardpintens@gmail.com>
2025-09-24 17:53:12 +0200

    Commit

--- week2/Onleesbare_code.java
+++ week2/Onleesbare_code.java
@@ -1,7 +1,19 @@
 package week2;
-
+import java.util.Scanner;
 public class Onleesbare_code {
     public static void main(String[] args) {
-        
+        /* 1.
+            int a = 0;
+            int b;
+            while ((b=a++ * 2) < 10) {
+                System.out.println(b);}*/
+
+        /* 2. Gaat een string blijven vragen tot je een lege geeft
+            String line;
+            Scanner keyboard = new Scanner(System.in);
+            while((line = keyboard.nextLine()).length() > 0) {
+                System.out.println("je gaf volgende string: " + line);
+            }
+            System.out.println("je gaf een lege string");*/
     }
 }
