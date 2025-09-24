45fd7743a882526b3b421a192ece2e465f670d44
wardpintens-spec <wardpintens@gmail.com>
2025-09-24 20:38:24 +0200

    Commit

--- week2/Codering.java
+++ week2/Codering.java
@@ -2,6 +2,41 @@ package week2;
 import java.util.Scanner;
 public class Codering {
     public static void main(String[] args) {
+        String bericht;
+        StringBuilder nieuwBericht = new StringBuilder();
+        String optie;
+        int lengte;
+        int newChar;
 
+        Scanner scan = new Scanner(System.in);
+
+        do {System.out.print("Geef een bericht: ");
+        bericht = scan.nextLine();}while (bericht.isEmpty());
+        lengte = bericht.length();
+
+        do {System.out.print("Wil je dit encoderen of decoderen? ");
+        optie = scan.nextLine();} while (!optie.equals("decoderen") && !optie.equals("encoderen"));
+
+        if (optie.equals("encoderen")){
+            for (int i = 0; i < lengte; i++){
+                if ((int) bericht.charAt(i) < 96){
+                newChar = (int) bericht.charAt(i);
+                nieuwBericht.append((char) newChar);}
+                else {newChar = (int) bericht.charAt(i) + 3;
+                    nieuwBericht.append((char) newChar);
+                }
+            }
+            System.out.println(nieuwBericht);
+        }
+        else {for (int i = 0; i < lengte; i++){
+                if ((int) bericht.charAt(i) < 96){
+                    newChar = (int) bericht.charAt(i);
+                    nieuwBericht.append((char) newChar);}
+                else {newChar = (int) bericht.charAt(i) - 3;
+                    nieuwBericht.append((char) newChar);
+                }
+            }
+        System.out.println(nieuwBericht);
+        }
     }
 }
