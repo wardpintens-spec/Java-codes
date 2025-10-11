45de4bc63abe985e8a3ef683ad6b64c4af01d817
wardpintens-spec <wardpintens@gmail.com>
2025-10-11 16:37:06 +0200

    Commit

--- week4/Dungeons_and_dragons/Dobbelsteen.java
+++ week4/Dungeons_and_dragons/Dobbelsteen.java
@@ -1,21 +1,13 @@
 package week4.Dungeons_and_dragons;
-import java.util.Random;
-import java.util.Scanner;
 
 public class Dobbelsteen {
     private int zijden;
 
+    public int getZijden() {
+        return zijden;
+    }
 
     public void setZijden(int zijden){
         this.zijden = zijden;
-    }
-
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Geef aantal zijden: ");
-
-
     }
 }
--- week4/Dungeons_and_dragons/Dungeons_and_dragons.java
+++ week4/Dungeons_and_dragons/Dungeons_and_dragons.java
@@ -1,6 +1,7 @@
 package week4.Dungeons_and_dragons;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Dungeons_and_dragons {
     public static int gooi(int zijden){
@@ -12,6 +13,19 @@ public class Dungeons_and_dragons {
     }
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Geef aantal zijden: ");
+        int zijden = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Druk op <enter> om een waarde te creëren");
+            scan.nextLine();
+            System.out.println(gooi(zijden));
+        }
+        System.out.println("Klaar.");
+
 
     }
 }
